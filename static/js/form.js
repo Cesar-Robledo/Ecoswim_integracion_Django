15361275057c655b0bce0c755478744b937f73df
Andreschkw <63243348+Andreschkw@users.noreply.github.com>
2023-07-14 19:04:33 -0300

    Update form.js
    Copiado de VS

--- static/js/form.js
+++ static/js/form.js
@@ -44,41 +44,46 @@ const selectProvincias = document.getElementById("selectProvincias");
       });
   });
 
-  document.getElementById("myForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Evitar el envío automático del formulario
+  var myForm = document.getElementById("myForm");
+  if (myForm) { 
 
-    const name = document.getElementsByName("name")[0].value;
-    const telefono = document.getElementsByName("telefono")[0].value;
-    const email = document.getElementsByName("email")[0].value;
-    const comentarios = document.getElementsByName("Comentarios")[0].value;
-    const provincia = selectProvincias.options[selectProvincias.selectedIndex].text;
-    const localidad = selectLocalidades.options[selectLocalidades.selectedIndex].text;
-    const modeloInteres = document.getElementsByName("modeloInteres")[0].value;
-    const formaPago = document.getElementsByName("formaPago")[0].value;
+    document.getElementById("myForm").addEventListener("submit", (event) => {
+      event.preventDefault(); // Evitar el envío automático del formulario */
 
-    // Construir los datos a enviar por correo
-    const formData = new FormData();
-    formData.append("name", name);
-    formData.append("telefono", telefono);
-    formData.append("email", email);
-    formData.append("Comentarios", comentarios);
-    formData.append("Provincia", provincia);
-    formData.append("Localidad", localidad);
-    formData.append("modeloInteres", modeloInteres);
-    formData.append("formaPago", formaPago);
+      const name = document.getElementsByName("name")[0].value;
+      const telefono = document.getElementsByName("telefono")[0].value;
+      const email = document.getElementsByName("email")[0].value;
+      const comentarios = document.getElementsByName("Comentarios")[0].value;
+      const provincia = selectProvincias.options[selectProvincias.selectedIndex].text;
+      const localidad = selectLocalidades.options[selectLocalidades.selectedIndex].text;
+      const modeloInteres = document.getElementsByName("modeloInteres")[0].value;
+      const formaPago = document.getElementsByName("formaPago")[0].value;
 
-    // Enviar los datos utilizando FormSubmit
-    fetch("https://formsubmit.co/robledoca@gmail.com", {
-      method: "POST",
-      body: formData
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log("Respuesta del envío por correo:", data);
-        // Aquí puedes realizar cualquier acción adicional después del envío, como mostrar un mensaje de confirmación al usuario
+      // Construir los datos a enviar por correo
+      const formData = new FormData();
+      formData.append("name", name);
+      formData.append("telefono", telefono);
+      formData.append("email", email);
+      formData.append("Comentarios", comentarios);
+      formData.append("Provincia", provincia);
+      formData.append("Localidad", localidad);
+      formData.append("modeloInteres", modeloInteres);
+      formData.append("formaPago", formaPago);
+
+      // Enviar los datos utilizando FormSubmit
+      fetch("https://formsubmit.co/robledoca@gmail.com", {
+        method: "POST",
+        body: formData
       })
-      .catch(error => {
-        console.error("Error al enviar el formulario:", error);
-        // Aquí puedes manejar cualquier error que ocurra durante el envío del formulario
-      });
-  });
+        .then(response => response.json())
+        .then(data => {
+          console.log("Respuesta del envío por correo:", data);
+          // Aquí puedes realizar cualquier acción adicional después del envío, como mostrar un mensaje de confirmación al usuario
+        })
+        .catch(error => {
+          console.error("Error al enviar el formulario:", error);
+          // Aquí puedes manejar cualquier error que ocurra durante el envío del formulario
+        });
+        
+    });
+  }
